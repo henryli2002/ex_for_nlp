a25599ab734bb2dba21934cbf3deccf1cfb17f02
lhy <3135601878@qqcom>
2024-05-18 17:03:45 +0800

    finish bert

--- TransNER/BERT.py
+++ TransNER/BERT.py
@@ -20,6 +20,7 @@ learning_rate = 5e-5
 max_length = 100
 num_labels = 6
 
+# 初始化berttokenizer
 tokenizer = BertTokenizer.from_pretrained(
     pretrained_model_name_or_path="bert-base-chinese",
     cache_dir=None,
@@ -57,11 +58,13 @@ def train(model, data_loader, optimizer, device, loss_fn=torch.nn.CrossEntropyLo
     print(f"Average loss: {avg_loss:.4f}")
 
 
-def test(model, data_loader, device, ignore_index=-1):
+# 训练函数
+def test(model, data_loader, device, idx_to_label, ignore_index=-1, save=0):
     model.eval()
     total_loss = 0
     true_labels = []
     predictions_list = []  
+    sentences = []
     loss_fn = torch.nn.CrossEntropyLoss(ignore_index=ignore_index)  
     progress_bar = tqdm(enumerate(data_loader), total=len(data_loader))
     with torch.no_grad():
@@ -81,25 +84,30 @@ def test(model, data_loader, device, ignore_index=-1):
             active_positions = labels != ignore_index
             true_labels.extend(labels[active_positions].cpu().numpy())
             predictions_list.extend(predictions[active_positions].cpu().numpy())
+        
+            for i in range(len(input_ids)):
+                sentence_labels = predictions[i][attention_mask[i] == 1].cpu().numpy()
+                sentences.append(" ".join(str(idx_to_label[label]) for label in sentence_labels))
     
     avg_loss = total_loss / len(data_loader)
     precision = precision_score(true_labels, predictions_list, average="weighted", zero_division=0)
     recall = recall_score(true_labels, predictions_list, average="weighted", zero_division=0)
     f1 = f1_score(true_labels, predictions_list, average="weighted", zero_division=0)
 
+    # 将结果写入文件
+    with open('./output/2021213688.txt', "w", encoding="utf-8") as f:
+        for sentence in sentences:
+            f.write(sentence + "\n")
+
     print(
         f"Test Loss: {avg_loss:.4f}, Precision: {precision:.4f}, Recall: {recall:.4f}, F1 Score: {f1:.4f}"
     )
 
-    return precision, true_labels, predictions_list
+    return precision, true_labels, predictions_list, sentences
 
 
 # encoder
 def prepare_data(data_path, label_path, labels_dict):
-    """对文本数据进行编码的函数
-    参数:
-
-    """
     data = []
     labels = []
 
@@ -178,38 +186,32 @@ def main():
             labels_set.update(line)
     labels_dict = {label: idx for idx, label in enumerate(sorted(labels_set))}
     print(labels_dict)
-    # train_path = "data/train.txt"
-    # train_TAG_path = "data/train_TAG.txt"
-    # train_data, train_labels = prepare_data(train_path, train_TAG_path, labels_dict)
+    train_path = "data/train.txt"
+    train_TAG_path = "data/train_TAG.txt"
+    train_data, train_labels = prepare_data(train_path, train_TAG_path, labels_dict)
 
-    # # 打印输入数据的键和形状
-    # print("Encoded data shapes:")
-    # for key, value in train_data.items():
-    #     print(f"{key}: {value.shape}")
+    # 打印输入数据的键和形状
+    print("Encoded data shapes:")
+    for key, value in train_data.items():
+        print(f"{key}: {value.shape}")
 
-    # # 打印标签数据的形状
-    # print("Labels shape:", train_labels.shape)
+    # 打印标签数据的形状
+    print("Labels shape:", train_labels.shape)
 
-    # dataset = TensorDataset(
-    #         train_data['input_ids'], 
-    #         train_data['attention_mask'], 
-    #         train_labels
-    #     )
+    dataset = TensorDataset(
+            train_data['input_ids'], 
+            train_data['attention_mask'], 
+            train_labels
+        )
     
-    # dataset_size = len(dataset)
-
-    # subset_size = int(0.01 * dataset_size)
-
-
-    # indices = list(range(dataset_size))
-
-    # np.random.shuffle(indices)
-
-    # subset_indices = indices[:subset_size]
-
-    # sampler = SubsetRandomSampler(subset_indices)
-
-    # data_loader = DataLoader(dataset, batch_size=batch_size, sampler=sampler)
+    # 准备训练参数，每轮从总数据中抽2000个数据训练
+    dataset_size = len(dataset)
+    subset_size = int(0.01 * dataset_size)
+    indices = list(range(dataset_size))
+    np.random.shuffle(indices)
+    subset_indices = indices[:subset_size]
+    sampler = SubsetRandomSampler(subset_indices)
+    data_loader = DataLoader(dataset, batch_size=batch_size, sampler=sampler)
     
 
 
@@ -220,47 +222,49 @@ def main():
 
     # 训练过程
     model.to(device)
-    # for epoch in range(num_epochs):
-    #     print(f"Epoch {epoch+1}/{num_epochs}")
-    #     train(model=model, data_loader=data_loader, optimizer=optimizer, device=device)
-    #     # 保存和加载模型，此处需要手动改模型路径
-    #     torch.save(model.state_dict(), f"./models/model_{epoch}e.pth")
+    for epoch in range(num_epochs):
+        print(f"Epoch {epoch+1}/{num_epochs}")
+        train(model=model, data_loader=data_loader, optimizer=optimizer, device=device)
+        # 保存和加载模型，此处需要手动改模型路径
+        torch.save(model.state_dict(), f"./models/model_{epoch}e.pth")
 
-    # # 验证过程
-    # best_ac = 0  # 记录最高值
-    # best_epoch = 0  # 记录最好epoch
-    # accuracy_per_epoch = []
-    # for epoch in range(num_epochs):
-    #     ac = 0
-    #     model.load_state_dict(torch.load(f"./models/model_{epoch}e.pth"))
-    #     dev_path = "data/dev.txt"
-    #     dev_TAG_path = "data/dev_TAG.txt"
-    #     dev_data, dev_labels = prepare_data(dev_path, dev_TAG_path, labels_dict)
+    # 验证过程
+    best_ac = 0  # 记录最高值
+    best_epoch = 0  # 记录最好epoch
+    accuracy_per_epoch = []
+    for epoch in range(num_epochs):
+        ac = 0
+        model.load_state_dict(torch.load(f"./models/model_{epoch}e.pth"))
+        dev_path = "data/dev.txt"
+        dev_TAG_path = "data/dev_TAG.txt"
+        dev_data, dev_labels = prepare_data(dev_path, dev_TAG_path, labels_dict)
 
-    #     dev_dataset = TensorDataset(
-    #         dev_data["input_ids"],
-    #         dev_data["attention_mask"],
-    #         dev_labels,
-    #     )
-    #     dev_data_loader = DataLoader(dev_dataset, batch_size=batch_size)
-    #     ac, true_labels, predictions_list = test(model, dev_data_loader, device)
-    #     accuracy_per_epoch.append(ac)
-    #     if ac > best_ac:
-    #         best_ac = ac
-    #         best_epoch = epoch
-    #     print(f"best_epoch:{best_epoch}")
+        dev_dataset = TensorDataset(
+            dev_data["input_ids"],
+            dev_data["attention_mask"],
+            dev_labels,
+        )
+        dev_data_loader = DataLoader(dev_dataset, batch_size=batch_size)
+        ac, true_labels, predictions_list = test(model, dev_data_loader, device)
+        accuracy_per_epoch.append(ac)
+        if ac > best_ac:
+            best_ac = ac
+            best_epoch = epoch
+        print(f"best_epoch:{best_epoch}")
 
-    # plt.figure(figsize=(10, 5))
-    # plt.plot(accuracy_per_epoch, marker='o', linestyle='-', color='b')
-    # plt.title('Accuracy over Epochs')
-    # plt.xlabel('Epoch')
-    # plt.ylabel('Accuracy')
-    # plt.grid(True)
-    # plt.xticks(range(num_epochs), [f"Epoch {i+1}" for i in range(num_epochs)]) 
-    # plt.savefig("./pic/accuracy_over_epochs.png")
-    # plt.show()
+    # 绘制验证图像
+    plt.figure(figsize=(10, 5))
+    plt.plot(accuracy_per_epoch, marker='o', linestyle='-', color='b')
+    plt.title('Accuracy over Epochs')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.grid(True)
+    plt.xticks(range(num_epochs), [f"Epoch {i+1}" for i in range(num_epochs)]) 
+    plt.savefig("./pic/accuracy_over_epochs.png")
+    plt.show()
 
-    # model.load_state_dict(torch.load(f"./models/model_{best_epoch}e.pth"))
+    # 测试模型性能
+    model.load_state_dict(torch.load(f"./models/model_{best_epoch}e.pth"))
     model.load_state_dict(torch.load(f"./models/model_{38}e.pth"))
     test_path = "data/test.txt"
     test_TAG_path = "data/test_TAG.txt"
@@ -272,7 +276,10 @@ def main():
         test_labels,
     )
     test_data_loader = DataLoader(test_dataset, batch_size=batch_size)
-    ac, true_labels, predictions_list = test(model, test_data_loader, device)
+    idx_to_label = {v: k for k, v in labels_dict.items()}
+    ac, true_labels, predictions_list, sentences = test(model, test_data_loader, device, idx_to_label, save=1)
+
+    # 绘制混淆矩阵和数据分布
     plot_metrics(true_labels, predictions_list, num_classes=len(labels_dict)) 
 
 
